ef7d52bef6776d206aa56b7b0c9192b56a271fa9
WrathRockeu <crnbrdrck@gmail.com>
2016-01-26 15:26:43 +0000

    GUI working, need to add more panels

---
+++ ITMInterface.java
@@ -0,0 +1,81 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.*;
+
+class ITMInterface {
+
+    static Frame mainFrame;
+    static Panel filePanel;
+    static TextField fileName;
+    static Button browse;
+    static JFileChooser fileChooser;
+    static FileNameExtensionFilter filter;
+    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public static void main(String[] args) {
+        //Create and initialise the frame
+        mainFrame = new Frame();
+        mainFrame.setResizable(false);
+        mainFrame.addWindowListener(new WindowAdapter() { public void windowClosing(WindowEvent we) {mainFrame.dispose();} } );
+        mainFrame.setTitle("Sonic Pictures");
+
+        //Create all the panels
+        initialiseFileDialog();
+        createFilePanel();
+
+        //Auto size and display window
+        mainFrame.pack();
+        centerFrame();
+        mainFrame.setVisible(true);
+
+    }
+
+    static void initialiseFileDialog() {
+        //Initialises but doesn't show the file window
+        fileChooser = new JFileChooser();
+        filter = new FileNameExtensionFilter(
+            "Images", "jpg", "png", "gif", "jpeg");
+        fileChooser.setFileFilter(filter);
+    }
+
+    static void createFilePanel() {
+        //Create the panel
+        filePanel = new Panel();
+        //Create the textfield to display the name
+        fileName = new TextField("filename");
+        //Create the browse button
+        browse = new Button("Browse");
+
+        //Now edit these things
+        fileName.setEditable(false);
+        browse.addActionListener(new ActionListener() { public void actionPerformed(ActionEvent ae){browseForFile();}});
+
+        //Add the components to the panel
+        filePanel.add(fileName);
+        filePanel.add(browse);
+
+        //Add the panel to the mainframe
+        mainFrame.add(filePanel);
+    }
+
+    static void browseForFile() {
+        int returnVal = fileChooser.showOpenDialog(null);
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " +
+                fileChooser.getSelectedFile().getName());
+        }
+    }
+
+    static void centerFrame() {
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+        int windowWidth = mainFrame.getWidth();
+        int windowHeight = mainFrame.getHeight();
+
+        int x = (screenWidth / 2) - (windowWidth / 2);
+        int y = (screenHeight / 2) - (windowHeight / 2);
+
+        mainFrame.setLocation(x, y);
+    }
+}
